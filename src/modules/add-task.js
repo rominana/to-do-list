fe11272f3c0de3753e9ba047b7a17aea5121a568
Romina <romina.pati@gmail.com>
2022-07-13 10:23:30 -0400

    Set up enviroment for testing

--- src/modules/add-task.js
+++ src/modules/add-task.js
@@ -7,24 +7,27 @@ const input = document.getElementById('new-task');
 const button = document.getElementById('add-task');
 const alert = document.getElementById('alert');
 
+const addTask = () => {
+  const toDoTasks = getData();
+  if (input.value) {
+    const task = new Task(input.value, toDoTasks.length + 1);
+    createLi(task);
+    toDoTasks.push(task);
+    localStorage.setItem('toDoData', JSON.stringify(toDoTasks));
+    input.value = '';
+  } else {
+    alert.innerHTML = 'Empty field!';
+    alert.style.display = 'block';
+    setTimeout(() => {
+      alert.style.display = 'none';
+    }, 1000);
+  }
+};
+
 const addTaskEvent = () => {
-  button.addEventListener('click', () => {
-    const toDoTasks = getData();
-    if (input.value) {
-      const task = new Task(input.value, toDoTasks.length + 1);
-      createLi(task);
-      toDoTasks.push(task);
-      // set data in the local storage
-      localStorage.setItem('toDoData', JSON.stringify(toDoTasks));
-      input.value = '';
-    } else {
-      alert.innerHTML = 'Empty field!';
-      alert.style.display = 'block';
-      setTimeout(() => {
-        alert.style.display = 'none';
-      }, 1000);
-    }
-  });
+  button.addEventListener('click', addTask);
 };
 
 export default addTaskEvent;
+
+export { addTask };
