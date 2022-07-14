8ea018cdd723f6316ae8044ada6c7cf2cf2a4ac5
Romina <romina.pati@gmail.com>
2022-07-14 11:56:46 -0400

    Pair programming edit task test

--- src/modules/__mocks__/get-data.js
+++ src/modules/__mocks__/get-data.js
@@ -1,6 +1,16 @@
 const getData = () => {
-  const data = [{ description: 'clean my room', completed: false, index: 1 }];
-  return data;
+  const data = global.localStorage.getItem('toDoData');
+  if (data) {
+    return JSON.parse(data);
+  }
+  return [];
 };
 
 export default getData;
+
+// const getData = () => {
+//   const data = [{ description: 'clean my room', completed: false, index: 1 }];
+//   return data;
+// };
+
+// export default getData;
---
+++ src/modules/__test__/edit-task.test.js
@@ -0,0 +1,15 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import editTask from '../edit-task.js';
+import getData from '../get-data.js';
+
+jest.mock('../get-data.js');
+
+test('Edit items on the list', () => {
+  global.localStorage.setItem('toDoData', '[{"description":"clean the bathroom","completed":false,"index":1}]');
+  editTask('task1', 'wash the car', false);
+  const storageData = getData();
+  expect(storageData[0].description).toBe('wash the car');
+});
